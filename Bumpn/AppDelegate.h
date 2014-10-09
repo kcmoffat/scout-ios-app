e860177099c43f33507366ae59ac54bddb2faef4
KASEY MOFFAT <kaseymoffat@KASEYs-MacBook-Pro.local>
2014-10-08 18:10:53 -0700

    Initial Commit

---
+++ Bumpn/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bumpn
+//
+//  Created by KASEY MOFFAT on 10/8/14.
+//  Copyright (c) 2014 KASEY MOFFAT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
